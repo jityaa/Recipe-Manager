51e7c0bfd1c1f65d1064d8fcccf7a78c4474c289
jityaa <148393682+jityaa@users.noreply.github.com>
2023-11-03 20:51:13 -0400

    create recipe class

---
+++ recipemanager.py
@@ -0,0 +1,8 @@
+class Recipe:
+    def __init__(self, name, ingredients, instructions, category, cook_time):
+        self.name = name
+        self.ingredients = ingredients
+        self.instructions = instructions
+        self.category = category
+        self.cook_time = cook_time
+
