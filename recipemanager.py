5d3bd7aa4f05793ee559ca15b3e8e19e78a78b19
jityaa <148393682+jityaa@users.noreply.github.com>
2023-11-03 21:56:31 -0400

    update to recipe class

--- recipemanager.py
+++ recipemanager.py
@@ -6,3 +6,20 @@ class Recipe:
         self.category = category
         self.cook_time = cook_time
 
+    def view(self):
+        recipe_info = {
+            "Name": self.name,
+            "Ingredients": self.ingredients,
+            "Instructions": self.instructions,
+            "Category": self.category,
+            "Cook Time (minutes)": self.cook_time
+        }
+        return recipe_info
+    
+    def edit(self, new_name, new_ingredients, new_instructions, new_category, new_cook_time):
+        self.name = new_name
+        self.ingredients = new_ingredients
+        self.instructions = new_instructions
+        self.category = new_category
+        self.cook_time = new_cook_time
+        print("Recipe updated.")
